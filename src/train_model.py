6669feabb98b4fd469ee101e01f29db6f81e36af
diegoib <diego.ibayo@gmail.com>
2022-08-10 09:39:45 +0200

    Cleaned census data

--- src/train_model.py
+++ src/train_model.py
@@ -1,14 +1,20 @@
 # Script to train machine learning model.
 
 from sklearn.model_selection import train_test_split
+import pandas as pd
+import logging
 
 # Add the necessary imports for the starter code.
 from ml.data import process_data
 from ml.model import train_model, compute_model_metrics, inference
 
+logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
+logger = logging.getLogger()
 
 # Add code to load in the data.
-
+def ingest_data():
+    df = pd.read_csv('../data/census.csv')
+     
 
 
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
@@ -28,6 +34,8 @@ X_train, y_train, encoder, lb = process_data(
     train, categorical_features=cat_features, label="salary", training=True
 )
 
+def go():
+    
 # Proces the test data with the process_data function.
 
 # Train and save a model.
