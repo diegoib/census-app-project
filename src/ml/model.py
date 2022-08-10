fa27c8577511e0912967a67070cd9f46d5353a49
diegoib <diego.ibayo@gmail.com>
2022-08-10 10:54:21 +0200

    Completed model.py and functions on src/train_model.py

--- src/ml/model.py
+++ src/ml/model.py
@@ -1,5 +1,5 @@
 from sklearn.metrics import fbeta_score, precision_score, recall_score
-
+from sklearn.linear_model import LogisticRegression
 
 # Optional: implement hyperparameter tuning.
 def train_model(X_train, y_train):
@@ -17,8 +17,10 @@ def train_model(X_train, y_train):
     model
         Trained machine learning model.
     """
-
-    pass
+    logreg = LogisticRegression()
+    logreg.fit(X_train, y_train)
+    
+    return logreg    
 
 
 def compute_model_metrics(y, preds):
@@ -57,4 +59,5 @@ def inference(model, X):
     preds : np.array
         Predictions from the model.
     """
-    pass
+    predictions = model.predict(X)
+    return predictions
--- src/train_model.py
+++ src/train_model.py
@@ -3,6 +3,8 @@
 from sklearn.model_selection import train_test_split
 import pandas as pd
 import logging
+import joblib
+import os
 
 # Add the necessary imports for the starter code.
 from ml.data import process_data
@@ -12,30 +14,65 @@ logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
 logger = logging.getLogger()
 
 # Add code to load in the data.
-def ingest_data():
-    df = pd.read_csv('../data/census.csv')
-     
+def ingest_data(path):
+    file_path = os.path.join(path, "data", "census.csv")
+    df = pd.read_csv(file_path)
+    return df
 
-
-# Optional enhancement, use K-fold cross validation instead of a train-test split.
-train, test = train_test_split(data, test_size=0.20)
-
-cat_features = [
-    "workclass",
-    "education",
-    "marital-status",
-    "occupation",
-    "relationship",
-    "race",
-    "sex",
-    "native-country",
-]
-X_train, y_train, encoder, lb = process_data(
-    train, categorical_features=cat_features, label="salary", training=True
-)
 
 def go():
-    
-# Proces the test data with the process_data function.
 
-# Train and save a model.
+    path = os.getcwd()
+
+    logger.info("Ingesting the data")
+    data = ingest_data(path)
+
+    logger.info("Splitting the data and processing it")
+    # Optional enhancement, use K-fold cross validation instead of a train-test split.
+    train, test = train_test_split(data, test_size=0.20)
+
+    cat_features = [
+        "workclass",
+        "education",
+        "marital-status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "native-country",
+    ]
+    X_train, y_train, encoder, lb = process_data(
+        train, categorical_features=cat_features, label="salary", training=True
+    )
+    # Proces the test data with the process_data function.
+    X_test, y_test, _, _ = process_data(
+        test, categorical_features=cat_features, label="salary", training=False
+    )
+
+    # Train and save a model.
+    logger.info("Training model and making inference")
+    model = train_model(X_train, y_train)
+    
+    preds = inference(model, X_test)
+    precision, recall, fbeta = compute_model_metrics(y_test, preds)
+    
+    logger.info(f"Precision: {:.2f}".format(precision))
+    logger.info(f"Recall: {:.2f}".format(recall))
+    logger.info(f"Fbeta: {:.2f}".format(fbeta))
+
+    # Dumps
+    logger.info("Dumping objects")    
+    model_path = os.path.join(path, 'model', 'finalized_model.sav')
+    joblib.dump(model, model_path)
+
+    cat_encoder_path = os.path.join(path, 'model', 'cat_encoder.sav')
+    joblib.dump(encoder, cat_encoder_path)
+    
+    lb_encoder_path = os.path.join(path, 'model', 'lb_encoder.sav')
+    joblib.dump(lb, lb_encoder_path)
+
+
+
+if __name__ == "__main__":
+    
+    go()
